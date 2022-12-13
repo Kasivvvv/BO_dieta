a6e22278f529e388b63ef8926aa1f68210835398
jakubwisla <80826207+jakubwisla@users.noreply.github.com>
2022-12-13 13:42:06 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,16 @@
 import random
-from classes import recipe
+from classes import recipe, storage
 
-def new_solution(recipes: list[recipe]):
-    solution = random.sample(recipes, 21)
+def new_solution(products: storage, recipes: list[recipe]):
+    to_use = len(products.products) // 2
+    while 1:
+        solution = random.sample(recipes, 21)
+        used_products = []
+        for recipe in solution:
+            for ingredient in recipe.ingredients:
+                if ingredient[0] in products.products.keys() and ingredient[0] not in used_products:
+                    used_products.append(ingredient[0])
+        if len(used_products) >= to_use:
+            break
     return solution
+
